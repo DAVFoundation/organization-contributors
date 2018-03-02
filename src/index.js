bb5c27d7fb88bf650c65bdf4d1cb257671ca0dcc
cg-cnu <sreenivas9alapati@gmail.com>
2018-03-03 01:30:47 +0530

    write ouput to json file

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const { EOL } = require('os');
 var pkg = require('../package.json');
 var conifg = require('./lib/config');
 var gh = require('./lib/github');
+var { writeFile } = require('fs');
 
 program.on('--help', () => {
   console.log(`
   Example:
   -  organization
-  $ ${pkg.name} -o davfoundation -t "abcdef" -c 10
+  $ ${pkg.name} -o davfoundation -t "abcdef" -c 10 -e exclude.json -w output.json
 
   Find out more at ${pkg.homepage}`);
 });
@@ -22,6 +23,7 @@ program
   .option('-t, --token <s>', 'GitHub token')
   .option('-c, --count <n>', 'total no of top users')
   .option('-e, --exclude <s>', 'exclude user/repo file path')
+  .option('-w, --write <s>', 'write output to the .json file')
   .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
@@ -31,7 +33,9 @@ if (!process.argv.slice(2).length) {
 gh.authenticate(program.token);
 gh.getOrgContributors(program.organization, program.count, program.exclude)
   .then(contributors => {
-    console.log(contributors);
+    writeFile(program.write, JSON.stringify(contributors), 'utf8', () => {
+      console.log(contributors);
+    });
   })
 
 console.log(`${pkg.name} v${pkg.version} - ${pkg.description}` + EOL);
